68d4087c8c2ea6c4e34381886f4f43133b3fe5e1
jonathanmorales990 <you@example.com>
2018-06-17 21:21:38 -0300

    React with webpack4

---
+++ src/main/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+	
+	render() {
+  	
+	    return (
+		    <div className='title'>
+				<h1>Hello World</h1>
+			</div>
+		)
+	    
+  	}
+}
+
+export default App;
