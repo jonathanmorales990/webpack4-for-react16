bc2b6d0fb7d3bf43c0b72923aa93c0446ebdd085
jonathanmorales990 <johnmorales990@gmail.com>
2020-02-08 21:05:48 -0200

    settings to extract css correctly

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './app.scss';
+import Navbar from '../components/Navbar.jsx';
 
 class App extends Component {
 	render() {
 		return (
-			<div className='title'>
-				<h1>Hello World</h1>
+			<div className='main'>
+				<Navbar />
 			</div>
 		)    
   	}
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
 const webpack = require('webpack')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production';
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-
-module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: __dirname + '/build',
-        filename: '[name].js',
-        chunkFilename: '[name].js'
-    },
-    devServer: {
-        port: 8080,
-        contentBase: './build',
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-        alias: {
-            modules: __dirname + '/node_modules',
+module.exports = env => {
+    return {
+        entry: './src/index.jsx',
+        output: {
+            path: __dirname + '/build',
+            filename: '[name].js',
+            chunkFilename: '[name].js'
+        },
+        devServer: {
+            port: 8080,
+            contentBase: './build',
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx'],
+            alias: {
+                modules: __dirname + '/node_modules',
+            }
+        },
+        plugins: [ 
+            new MiniCssExtractPlugin({
+              filename: '[name].css',
+              chunkFilename: '[id].css',
+            })
+        ],
+        module: {
+            rules: [{
+                test: /.js[x]?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: ['transform-object-rest-spread']
+                }
+            },{
+                test: /\.(sa|sc|c)ss$/,
+                loader: [
+                    env !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },{
+                test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'media/'
+                }
+            }]
         }
-    },
-    plugins: [ 
-        new MiniCssExtractPlugin({
-          filename: devMode ? '[name].css' : '[name].[hash].css',
-          chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-        })
-    ],
-    module: {
-        rules: [{
-            test: /.js[x]?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react'],
-                plugins: ['transform-object-rest-spread']
-            }
-        },  {
-            test: /\.(sa|sc|c)ss$/,
-            loader: [
-              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-              'css-loader',
-              'postcss-loader',
-              'sass-loader'
-            ],
-        }, {
-            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-            loader: 'file-loader',
-            options: {
-                outputPath: 'media/'
-            }
-        }]
     }
 }
